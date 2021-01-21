494098bbaf32c08a6bbda7cc4c133b1526f6ed3d
Aniket Singh <aniket@factech.Dlink>
2021-01-21 11:21:26 +0530

    update task in second column

--- src/initial-data.js
+++ src/initial-data.js
@@ -8,10 +8,12 @@ const initialData = {
         'task-6': { id: 'task-6', content: 'Hello Task 6 Data' },
         'task-7': { id: 'task-7', content: 'Hello Task 7 Data' },
         'task-8': { id: 'task-8', content: 'Hello Task 8 Data' },
+        'task-9': { id: 'task-9', content: 'Hello Task 9 Data' },
+        'task-10': { id: 'task-10', content: 'Hello Task 10 Data' },
     },
     columns: {
         'column-1': { id: 'column-1', title: 'To Do', taskIds: ['task-1', 'task-2', 'task-3', 'task-4','task-5', 'task-6', 'task-7', 'task-8'] },
-        'column-2': { id: 'column-2', title: 'In Progress', taskIds: [] },
+        'column-2': { id: 'column-2', title: 'In Progress', taskIds: ['task-9', 'task-10',] },
         'column-3': { id: 'column-3', title: 'Done', taskIds: [] },
     },
     columnOrder: ['column-1', 'column-2', 'column-3'],
